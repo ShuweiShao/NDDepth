fa753a9982ef50322738e282dbc669504a9f43bb
Daoxin Zhong <daoxin.zhong@hmc.ox.ac.uk>
2024-11-08 08:37:17 +0000

    run inference

---
+++ infer.sh
@@ -0,0 +1,4 @@
+python ./Estimation/nddepth_custom/inference_single_image.py \
+    --dataset nyu \
+    --image_path /scratchdata/nyu_depth_v2/official_splits/test/kitchen/rgb_00201.jpg \
+    --checkpoint_path /scratchdata/test.pth
